8d1445ee0fb5e6feb6c68655319d7826ce60ad47
Rohini Kumar D <rohini@resourceone.in>
2020-12-03 14:15:55 +0530

    ip

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('==== request IP === ', req.ip)
+  console.log('==== request IP === ',req.ip.toString().replace('::ffff:', ''))
   res.render('index', { title: 'Express' });
 });
 
